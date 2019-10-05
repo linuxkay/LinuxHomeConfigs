10ddd58faf3275e38488dec17837431923dcd31c
Linuxkay <sccck000@gmail.com>
2019-10-05 21:10:16 +0900

    added installed app for Ubuntu 18.04LTS

---
+++ installed.sh
@@ -0,0 +1,5 @@
+sudo apt -y install ffmpeg audacity nmap powertop aircrack-ng openssl wifite pyrit libssl-ocaml-dev libcurl4-openssl-dev  libnetfilter-queue-dev libusb-1.0-0-dev libpcap-dev ttf-mscorefonts-installer gnome-shell-timer xsane g++ cmake apt-transport-https ca-certificates  curl  gnupg-agent software-properties-common  ansible zsh lib32z1  xboxdrv john git gcc make libpcap-dev telnet tmux redis-server pinta  imagemagick adb conky fonts-takao-gothic fonts-takao fonts-takao-mincho  fonts-takao-pgothic fonts-ipafont-gothic hexedit ghex unrar unzip bchunk python3-pip htop vim thinkfan gimp inkscape gnome-clocks  terminator filezilla vlc thunar gnome-tweaks openvpn ubuntu-restricted-extras tlp
+sudo systemctl enable thinkfan
+sudo systemctl enable tlp 
+sudo systemctl enable openvpn-client@client 
+
